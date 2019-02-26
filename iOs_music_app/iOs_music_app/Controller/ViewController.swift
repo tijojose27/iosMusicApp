c129a6800b6c1be8f53412a91d9ccf076ba7e424
TJ <tijojose27@yahoo.com>
2019-02-25 23:22:22 -0600

    UI DONE

---
+++ iOs_music_app/iOs_music_app/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iOs_music_app
+//
+//  Created by Tijo Jose on 2/24/19.
+//  Copyright © 2019 Tijo Jose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var myMusic : [Music] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+
+}
+
---
+++ iOs_music_app/iOs_music_app/Model/Music.swift
@@ -0,0 +1,17 @@
+//
+//  Music.swift
+//  iOs_music_app
+//
+//  Created by Tijo Jose on 2/24/19.
+//  Copyright © 2019 Tijo Jose. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Music{
+    var artist : String?
+    var album : String?
+    var song : String?
+    var image : UIImage?
+}
